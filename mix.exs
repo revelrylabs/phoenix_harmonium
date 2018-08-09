c63beb8214f46fe03f2a0afac9e216665119ecf3
jwietelmann <jwietelmann@gmail.com>
2018-08-09 11:45:09 -0400

    bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Harmonium.MixProject do
   def project do
     [
       app: :harmonium,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
