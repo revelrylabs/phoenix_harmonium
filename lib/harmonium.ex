b5813f75e886c9969718e027d71340623371f090
Brian Berlin <bri@nberl.in>
2019-04-03 13:38:32 -0500

    adds example for how to specify selected options

--- lib/harmonium.ex
+++ lib/harmonium.ex
@@ -402,6 +402,9 @@ defmodule Harmonium do
     iex> select_stack(f, :required_string, ["Hi": "hi", "Hello": "hello"]) |> safe_to_string()
     "<label class=\\\"rev-InputLabel rev-SelectStack \\\">  \\n  <select class=\\\"rev-Select \\\" id=\\\"widget_required_string\\\" name=\\\"widget[required_string]\\\"><option value=\\\"hi\\\">Hi</option><option value=\\\"hello\\\" selected>Hello</option></select>\\n  \\n  \\n</label>"
 
+    iex> select_stack(f, :required_string, ["Hi": "hi", "Hello": "hello"], input: [value: "hello"]) |> safe_to_string()
+    "<label class=\"rev-InputLabel rev-SelectStack \">  \n  <select class=\"rev-Select \" id=\"widget_required_string\" name=\"widget[required_string]\"><option value=\"hi\">Hi</option><option value=\"hello\" selected>Hello</option></select>\n  \n  \n</label>"
+
   See `text_input_stack/3` for more options.
   """
   def select_stack(f, key, value_options, options \\ []) do
@@ -413,6 +416,9 @@ defmodule Harmonium do
 
     iex> multiple_select_stack(f, :required_string, ["Hi": "hi", "Hello": "hello"], label: "Greetings", input: [required: true]) |> safe_to_string()
     "<label class=\\\"rev-InputLabel rev-SelectStack \\\">  <span class=\\\"rev-LabelText\\\">Greetings</span>\\n  <select class=\\\"rev-Select \\\" id=\\\"widget_required_string\\\" multiple=\\\"\\\" name=\\\"widget[required_string][]\\\" required><option value=\\\"hi\\\">Hi</option><option value=\\\"hello\\\" selected>Hello</option></select>\\n  \\n  \\n</label>"
+
+    iex> multiple_select_stack(f, :required_string, ["Hi": "hi", "Hello": "hello"], input: [value: ["hi", "hello"]]) |> safe_to_string()
+    "<label class=\"rev-InputLabel rev-SelectStack \">  \n  <select class=\"rev-Select \" id=\"widget_required_string\" multiple=\"\" name=\"widget[required_string][]\"><option value=\"hi\" selected>Hi</option><option value=\"hello\" selected>Hello</option></select>\n  \n  \n</label>"
 
   See `text_input_stack/3` for more options.
   """
