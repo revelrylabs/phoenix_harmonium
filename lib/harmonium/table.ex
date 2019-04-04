9856fbb7d4e380c44ee6f1264ac19611bbdbd886
Bryan Joseph <bryanjos@users.noreply.github.com>
2019-04-04 07:14:15 -0500

    Refactor table module

--- lib/harmonium/table.ex
+++ lib/harmonium/table.ex
@@ -4,49 +4,33 @@ defmodule Harmonium.Table do
   import Phoenix.HTML.Tag
 
   def table(opts \\ [], do: block) do
-    opts = Keyword.merge([class: "rev-Table"], opts)
-
-    content_tag :table, opts do
-      block
-    end
+    table_element(:table, [class: "rev-Table"], opts, do: block)
   end
 
   def table_head(opts \\ [], do: block) do
-    opts = Keyword.merge([class: "rev-Table-head"], opts)
-
-    content_tag :thead, opts do
-      block
-    end
+    table_element(:thead, [class: "rev-Table-head"], opts, do: block)
   end
 
   def table_body(opts \\ [], do: block) do
-    opts = Keyword.merge([class: "rev-Table-body"], opts)
-
-    content_tag :tbody, opts do
-      block
-    end
+    table_element(:tbody, [class: "rev-Table-body"], opts, do: block)
   end
 
   def table_row(opts \\ [], do: block) do
-    opts = Keyword.merge([class: "rev-Table-row"], opts)
-
-    content_tag :tr, opts do
-      block
-    end
+    table_element(:tr, [class: "rev-Table-row"], opts, do: block)
   end
 
   def table_header(opts \\ [], do: block) do
-    opts = Keyword.merge([class: "rev-Table-header"], opts)
-
-    content_tag :th, opts do
-      block
-    end
+    table_element(:th, [class: "rev-Table-header"], opts, do: block)
   end
 
   def table_data(opts \\ [], do: block) do
-    opts = Keyword.merge([class: "rev-Table-Data"], opts)
+    table_element(:td, [class: "rev-Table-Data"], opts, do: block)
+  end
 
-    content_tag :td, opts do
+  def table_element(tag, default_opts, opts, do: block) do
+    opts = Keyword.merge(default_opts, opts)
+
+    content_tag tag, opts do
       block
     end
   end
